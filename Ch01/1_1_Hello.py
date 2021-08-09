9438eb315c2d1ef1e2b1bb19c475f8ba12569902
bakeunbi99 <peb7949@naver.com>
2021-08-09 18:08:59 +0900

    2021/08/09 파이썬 실습예제

---
+++ Ch01/1_1_Hello.py
@@ -0,0 +1,15 @@
+"""
+날짜 : 2021/08/09 15:06
+이름 : 박은비
+내용 : 파이썬 개발환경 설정 교재 p15
+"""
+
+print('Hello World~')
+print('Hello Python!')
+
+# end 옵션을 이용한 한줄 출력
+print('Hello', end='!')
+print('Korea!')
+
+
+
